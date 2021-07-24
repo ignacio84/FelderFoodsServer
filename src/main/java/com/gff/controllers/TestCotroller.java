38eb6bd3349d08f3eb29d0f7946b7a40188d00e0
Ignacio Castro <ignacio.cm84@gmail.com>
2021-07-24 13:08:44 -0600

    se crea controlador TestController para hacer pruebas de seguridad

--- src/main/java/com/gff/controllers/TestCotroller.java
+++ src/main/java/com/gff/controllers/TestCotroller.java
@@ -5,6 +5,7 @@ import com.gff.models.entity.app.Usuario;
 import com.gff.models.services.app.UsuarioService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class TestCotroller {
     @Autowired
     private UsuarioService usuarioService;
     
+    @Secured({"ROLE_ADMIN", "ROLE_PROD"})
     @GetMapping("/usuario")
     public Usuario menu() {
         return this.usuarioService.findByUserName("icastro");
